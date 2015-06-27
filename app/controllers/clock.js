7eecbbb3b73f0cf5eaa0e1a9ffa9e22cf67e5469
ambirdsall <ambirdsall@gmail.com>
2015-06-27 00:08:01 -0700

    Updates the time displayed every second.

--- app/controllers/clock.js
+++ app/controllers/clock.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  init: function() {
+    this.updateTime();
+  },
+  updateTime: function() {
+    var _this = this;
+
+    //Update the time every second
+    Ember.run.later(function(){
+      _this.set('localTime', new Date().toLocaleTimeString());
+      _this.updateTime();
+    }, 1000);
+  },
   localTime: new Date().toLocaleTimeString()
 });
