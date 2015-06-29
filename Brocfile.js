3b117236d2073518afe614b6067bab88bd5473be
ambirdsall <ambirdsall@gmail.com>
2015-06-29 15:21:04 -0700

    Adds moment's timezone library.

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/moment/moment.js');
+app.import('bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js');
+
 module.exports = app.toTree();
