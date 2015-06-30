e8b997882fb572dc1b5af837414d01cbc5849533
ambirdsall <ambirdsall@gmail.com>
2015-06-29 21:48:10 -0700

    Creates model for timezones and updates all timezones along w/ local clock.

--- app/controllers/clock.js
+++ app/controllers/clock.js
@@ -9,9 +9,12 @@ export default Ember.Controller.extend({
 
     //Update the time every second
     Ember.run.later(function(){
-      _this.set('localTime', new Date().toLocaleTimeString());
+      _this.set('localTime', moment().format('h:mm:ss a'));
+      _this.get('model').forEach(function(model) {
+        model.set('time', moment().tz(model.get('name')).format('h:mm:ss a'));
+      });
       _this.updateTime();
     }, 1000);
-  },
+  }.on('init'),
   localTime: new Date().toLocaleTimeString()
 });
